5e683e5cf7031a875a204c89548ba31185052ac8
_ArN0b-- <arnobxtreme@gmail.com>
2022-08-04 23:30:54 +0600

    docker install updated

--- package.sh
+++ package.sh
@@ -104,13 +104,13 @@ do
 
             sudo apt update
             
-            sudo apt install apt-transport-https ca-certificates curl software-properties-common
-            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-            sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+            sudo apt-get install ca-certificates curl gnupg lsb-release
+            sudo mkdir -p /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
             
-            apt-cache policy docker-ce
-            
-            sudo apt install docker-ce
+            sudo apt-get update
+            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
             
             sudo systemctl --no-pager status docker
             sudo apt autoremove
